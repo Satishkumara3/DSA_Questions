380b57b9b55af5228d72e49bae70b0574b4d375c
Satish Kumar <150000733+Satishkumara3@users.noreply.github.com>
2026-08-07 23:46:01 +0530

    Time: 0 ms (100%), Space: 44.5 MB (95.17%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,7 +1,32 @@
 class Solution {
+
+    public int minCostClimbingStairs(int[] cost) {
+
+        int n=cost.length;
+        int dp[] =new int[n];
+
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+
+        for(int i=2;i<n;i++){
+            dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
+        }
+        return Math.min(dp[n-1],dp[n-2]);
+       
+    }
+}
+
+
+
+
+
+
+
+/*
+class Solution {
     int[] dp;
     public  int helper(int n,int[] cost){
-        if(n<=1){
+        if(n==1 || n==0){
             return 0;
         }
 
@@ -20,7 +45,7 @@ class Solution {
 }
 
 
-
+*/
 
 
 
