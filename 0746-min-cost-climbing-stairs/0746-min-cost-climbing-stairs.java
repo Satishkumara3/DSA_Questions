30a0155600f39bbcef2a54710629209f10d910bd
Satish Kumar <150000733+Satishkumara3@users.noreply.github.com>
2026-08-07 17:16:10 +0530

    Time: 1 ms (39.28%), Space: 44.7 MB (76.31%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,4 +1,31 @@
 class Solution {
+    int[] dp;
+    public  int helper(int n,int[] cost){
+        if(n<=1){
+            return 0;
+        }
+
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+
+        dp[n]=+Math.min(helper(n-1,cost)+cost[n-1],helper(n-2,cost)+cost[n-2]);
+        return dp[n];
+    }
+    public int minCostClimbingStairs(int[] cost) {
+        dp=new int[cost.length+1];
+        Arrays.fill(dp,-1);
+        return helper(cost.length,cost);
+    }
+}
+
+
+
+
+
+
+/*
+class Solution {
     static int[] dp;
     public static int helper(int n,int[] arr){
         if(n>=arr.length){
@@ -18,3 +45,5 @@ class Solution {
         return Math.min(helper(0,cost),helper(1,cost));
     }
 }
+
+*/
