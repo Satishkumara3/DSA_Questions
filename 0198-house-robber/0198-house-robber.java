ef45c4f46e2aa179def9384e94e18e73a7a54f54
Satish Kumar <150000733+Satishkumara3@users.noreply.github.com>
2026-08-09 02:26:42 +0530

    Time: 0 ms (100%), Space: 42.1 MB (99.68%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,24 +1,53 @@
-class Solution {
-    int dp[];
-    public int helper(int[] nums,int i){
-        if(i>=nums.length) return 0;
+// class Solution {
+//     int dp[];
+//     public int helper(int[] nums,int i){
+//         if(i>=nums.length) return 0;
 
-        if(dp[i]!=-1){
-            return dp[i];
-        }
+//         if(dp[i]!=-1){
+//             return dp[i];
+//         }
 
         
 
-        int take=nums[i]+helper(nums,i+2);
-        int nottake=helper(nums,i+1);
+//         int take=nums[i]+helper(nums,i+2);
+//         int nottake=helper(nums,i+1);
 
-        return  dp[i]=Math.max(take,nottake);
-    }
+//         return  dp[i]=Math.max(take,nottake);
+//     }
+
+//     public int rob(int[] nums) {
+//         dp=new int[nums.length];
+//         Arrays.fill(dp,-1);
+
+//         return helper(nums,0);
+//     }
+// }
+
+
+// Tabulation method 
+
+
+
+class Solution {
+  
 
     public int rob(int[] nums) {
-        dp=new int[nums.length];
-        Arrays.fill(dp,-1);
+        int n=nums.length;
+        if(n==1){
+            return nums[0];
+        }
+        int[] dp=new int[n];
 
-        return helper(nums,0);
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+
+
+
+        for(int i=2;i<n;i++){
+
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+
+        }
+        return dp[n-1];
     }
 }
