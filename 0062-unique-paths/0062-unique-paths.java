4350ebc49452ac423c79d8bf91acd7de82a72cd5
Satish Kumar <150000733+Satishkumara3@users.noreply.github.com>
2026-08-11 01:36:40 +0530

    Time: 0 ms (100%), Space: 42.3 MB (13.09%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,33 @@
+class Solution {
+    static int[][] dp;
+    public static int ways(int i,int j,int m,int n){
+        if(i >=m || j>=n){
+            return 0;
+        }
+        if(i==m-1 && j==n-1){
+            return 1;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        
+
+        
+        dp[i][j]=ways(i+1,j,m,n)+ways(i,j+1,m,n);
+        return dp[i][j];
+
+
+
+
+
+    }
+    public int uniquePaths(int m, int n) {
+        dp=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
+            }
+        }
+        return ways(0,0,m,n);
+    }
+}
